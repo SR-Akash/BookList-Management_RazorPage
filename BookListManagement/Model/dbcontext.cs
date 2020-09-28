d6829c327069e3ee40e4554906b8e168c45c29af
SR-Akash <akashtah25@gmail.com>
2020-09-28 13:43:03 +0600

    First Commit

---
+++ BookListManagement/Model/dbcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListManagement.Pages.Model
+{
+    public class dbcontext:DbContext
+    {
+        public dbcontext(DbContextOptions<dbcontext>options) : base (options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
